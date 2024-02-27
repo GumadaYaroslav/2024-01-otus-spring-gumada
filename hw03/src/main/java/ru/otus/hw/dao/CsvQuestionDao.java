2f95413dc766dc448264e82abd3062a0ecc52a77
ygumada <ygumada@ozon.ru>
2024-02-27 23:32:35 +0300

    hw03

---
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.dao;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class CsvQuestionDao implements QuestionDao {
+    private final TestFileNameProvider fileNameProvider;
+
+    @Override
+    public List<Question> findAll() {
+        try {
+            InputStream questionsCsvStream = getClass().getClassLoader()
+                    .getResourceAsStream(fileNameProvider.getTestFileName());
+            Reader questionsFR = new InputStreamReader(questionsCsvStream);
+            CsvToBean<QuestionDto> build = new CsvToBeanBuilder(questionsFR)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1).build();
+
+            return build.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (Exception e) {
+            throw new QuestionReadException("Error wile reading questions", e);
+        }
+    }
+}
---
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,52 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+    public static final String CHOOSE_ANSWER_CODE = "TestService.choose.the.answer";
+    public static final String INVALID_ANSWER_CODE = "TestService.choose.invalid.answer";
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        ioService.printLine("");
+
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question : questions) {
+            ioService.printFormattedLine(question.text());
+            printAnswers(question);
+            testResult.applyAnswer(question, askQuestion(question));
+        }
+        return testResult;
+    }
+
+    private boolean askQuestion(Question question) {
+        int max = question.answers().size() - 1;
+        int answerNumberFromUser = ioService.readIntForRangeWithPromptLocalized(0,
+                max,
+                CHOOSE_ANSWER_CODE,
+                INVALID_ANSWER_CODE);
+        return question.answers().get(answerNumberFromUser - 1).isCorrect();
+    }
+
+    private void printAnswers(Question question) {
+        for (int numberOfQuestion = 1; numberOfQuestion <= question.answers().size(); numberOfQuestion++) {
+            ioService.printLine("%d. ".formatted(numberOfQuestion) +
+                    question.answers().get(numberOfQuestion - 1).text());
+        }
+    }
+
+}
